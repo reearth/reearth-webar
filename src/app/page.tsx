78ba1c37f4ee190bef6519a8dada758131fcd92f
Masaya Tsuruta <seinwesen@gmail.com>
2024-01-28 00:55:12 +0900

    UI Sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Script from "next/script"
 import ARView from "./ARView"
+import { ThemeProvider } from "@mui/material"
+import { AppBar, darkTheme } from "./components/prototypes/ui-components"
 
 export default function Home() {
   return (
-    <main>
-      <Script src="https://cesium.com/downloads/cesiumjs/releases/1.107.1/Build/Cesium/Cesium.js" />
-      <ARView />
-      <Script src="/ar.js" />
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <main>
+        <Script src="https://cesium.com/downloads/cesiumjs/releases/1.107.1/Build/Cesium/Cesium.js" />
+        <AppBar></AppBar>
+        <ARView />
+        <Script src="/ar.js" />
+      </main>
+    </ThemeProvider>
   )
 }
