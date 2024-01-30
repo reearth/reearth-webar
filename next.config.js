395dd0d6193a4d41b42d838a2a74df933371cac6
Masaya Tsuruta <seinwesen@gmail.com>
2024-01-31 02:09:34 +0900

    Copy Cesium asset files to public dir when build to avoid Error: Not allowed to load local resource.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,59 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+// for Cesium static asset files
+// https://github.com/willwill96/cesium-nextjs-example/blob/master/next.config.js
+// https://community.cesium.com/t/cesium-nextjs/27658/5
+// https://cesium.com/learn/cesiumjs-learn/cesiumjs-quickstart/
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const nextConfig = {
+    webpack: (config, { webpack, isServer }) => {
+      if (!isServer) {
+        config.plugins.push(
+          new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Workers'
+                ),
+                to: '../public/Cesium/Workers',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/ThirdParty'
+                ),
+                to: '../public/Cesium/ThirdParty',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Assets'
+                ),
+                to: '../public/Cesium/Assets',
+              },
+              {
+                from: path.join(
+                  __dirname,
+                  'node_modules/cesium/Build/Cesium/Widgets'
+                ),
+                to: '../public/Cesium/Widgets',
+              },
+            ],
+          })
+        )
+      }
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify('/Cesium'),
+        })
+      )
+      config.resolve.exportsFields = []
+      return config
+    },
+}
+
+// const nextConfig = {}
 
 module.exports = nextConfig
