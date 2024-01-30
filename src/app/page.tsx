78d194fd21d608a84412a18885d0e3d09987bc48
Masaya Tsuruta <seinwesen@gmail.com>
2024-01-31 02:37:45 +0900

    Use jotai Provider to access jotai singleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import { ThemeProvider } from "@mui/material"
 import { darkTheme } from "./components/prototypes/ui-components"
 import { AppHeader } from "./components/prototypes/view/ui-containers/AppHeader"
 import ARView from "./ARView"
+import { Provider } from "jotai"
 
 export default function Home() {
   return (
     <ThemeProvider theme={darkTheme}>
       <main className="flex flex-col">
-        <AppHeader />
-        <ARView className="relative w-screen h-full" />
+        <Provider>
+          <AppHeader />
+        </Provider>
+        <Provider>
+          <ARView className="relative w-screen h-full" />
+        </Provider>
       </main>
     </ThemeProvider>
   )
