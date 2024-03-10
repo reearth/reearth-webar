774460572456b1e128db0bc0b367f8c711c755e4
Masaya Tsuruta <seinwesen@gmail.com>
2024-03-10 17:35:51 +0900

    wip

--- src/ARView.tsx
+++ src/ARView.tsx
@@ -3,9 +3,36 @@ import { startAR, stopAR, resetTileset, updateCompassBias, updateFov } from "./a
 import { useAtom, useAtomValue } from "jotai";
 import queryString from "query-string";
 import { useDatasetById, useDatasetsByIds } from "./components/shared/graphql";
-import { PlateauDatasetItem } from "./components/shared/graphql/types/catalog";
+import { PlateauDataset, PlateauDatasetItem } from "./components/shared/graphql/types/catalog";
 import { rootLayersAtom } from "./components/shared/states/rootLayer";
 import { compassBiasAtom, fovPiOverAtom } from "./components/prototypes/view/states/ar";
+
+const tilesetUrls = (nodes) => {
+  nodes.map(node => {
+    const plateauDataset = node;
+    const plateauDatasetItems = plateauDataset.items as [PlateauDatasetItem];
+    // LOD2(テクスチャあり)->LOD2(テクスチャなし)->LOD1の順でフォールバック
+    const tilesetUrlLod2Tex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "TEXTURE").url
+    if (tilesetUrlLod2Tex) {
+      console.log("LOD2 with Texture Tileset Exists: ", tilesetUrlLod2Tex);
+      return tilesetUrlLod2Tex;
+    } else {
+      const tilesetUrlLod2NoneTex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "NONE").url
+      if (tilesetUrlLod2NoneTex) {
+        console.log("LOD2 with No Texture Tileset Exists: ", tilesetUrlLod2NoneTex);
+        return tilesetUrlLod2NoneTex;
+      } else {
+        const tilesetUrlLod1 = plateauDatasetItems.find(({ lod }) => lod == 1).url
+        if (tilesetUrlLod1) {
+          console.log("LOD1 Tileset Exists: ", tilesetUrlLod1);
+          return tilesetUrlLod1;
+        } else {
+          return null;
+        }
+      }
+    }
+  });
+}
 
 export default function ARView({...props}) {
   // 初期化フラグ
@@ -15,47 +42,20 @@ export default function ARView({...props}) {
   // データセットパネルに追加されたレイヤー群
   const [rootLayers, setRootLayers] = useAtom(rootLayersAtom);
 
+  // 開始時にクエパラでデータセットIDを指定された場合にARViewの初期化に使用するtilesetURL
+  let initialTilesetUrls: string[];
+
   // クエパラを見てPLATEAU ViewからのデータセットIDの初期値が来ていれば取得
   const searchQueryParams = queryString.parse(location.search, {arrayFormat: 'comma'});
   let initialDatasetIds = searchQueryParams.id;
   if (typeof initialDatasetIds === 'string') { initialDatasetIds = [initialDatasetIds]; }
-  // 
-  const [datasetIds, setDatasetIds] = useState([]);
-
-  const tilesetUrls = (nodes) => {
-    nodes.map(node => {
-      const plateauDataset = node;
-      const plateauDatasetItems = plateauDataset.items as [PlateauDatasetItem];
-      // LOD2(テクスチャあり)->LOD2(テクスチャなし)->LOD1の順でフォールバック
-      const tilesetUrlLod2Tex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "TEXTURE").url
-      if (tilesetUrlLod2Tex) {
-        console.log("LOD2 with Texture Tileset Exists: ", tilesetUrlLod2Tex);
-        return tilesetUrlLod2Tex;
-      } else {
-        const tilesetUrlLod2NoneTex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "NONE").url
-        if (tilesetUrlLod2NoneTex) {
-          console.log("LOD2 with No Texture Tileset Exists: ", tilesetUrlLod2NoneTex);
-          return tilesetUrlLod2NoneTex;
-        } else {
-          const tilesetUrlLod1 = plateauDatasetItems.find(({ lod }) => lod == 1).url
-          if (tilesetUrlLod1) {
-            console.log("LOD1 Tileset Exists: ", tilesetUrlLod1);
-            return tilesetUrlLod1;
-          }
-        }
-      }
-    });
-  }
-
   if (initialDatasetIds) {
     console.log(initialDatasetIds);
-    // setDatasetIds([...datasetIds, initialDatasetIds]);
-    // console.log(datasetIds);
-    const initialDatasets = useDatasetsByIds(initialDatasetIds).data;
+    // const initialDatasets = useDatasetsByIds(initialDatasetIds).data;
+    // console.log(initialDatasets);
+    const initialDatasets = useDatasetById(initialDatasetIds[0]).data;
     console.log(initialDatasets);
-    //const tilesetUrls = initialDatasets.nodes.flatMap(node => node.items.map(item => item.url));
-    const urls = tilesetUrls(initialDatasets.nodes);
-    //resetTileset(tilesetUrls);
+    initialTilesetUrls = tilesetUrls(initialDatasetIds);
   }
 
   // UIのステート
@@ -82,42 +82,22 @@ export default function ARView({...props}) {
   // }, []);
 
   // データセットパネルのレイヤー群が変化したらARViewを再レンダリング
-  useEffect(() => {
-    const datasetIds = rootLayers.map(rootLayer => rootLayer.rawDataset.id);
-    setDatasetIds(datasetIds);
-  }, [rootLayers]);
+  // useEffect(() => {
+  //   const datasetIds = rootLayers.map(rootLayer => rootLayer.rawDataset.id);
+  //   setDatasetIds(datasetIds);
+  // }, [rootLayers]);
 
   // AR View 起動
   useEffect(() => {
-    if (!cesiumLoaded || !data?.node) { return; }
-    const plateauDataset = data.node;
-    const plateauDatasetItems = plateauDataset.items as [PlateauDatasetItem];
-    // LOD2(テクスチャあり)->LOD2(テクスチャなし)->LOD1の順でフォールバック
-    const tilesetUrlLod2Tex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "TEXTURE").url
-    if (tilesetUrlLod2Tex) {
-      console.log("LOD2 with Texture Tileset Exists: ", tilesetUrlLod2Tex);
-      startAR(tilesetUrlLod2Tex);
-      setIsARStarted(true);
-    } else {
-      const tilesetUrlLod2NoneTex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "NONE").url
-      if (tilesetUrlLod2NoneTex) {
-        console.log("LOD2 with No Texture Tileset Exists: ", tilesetUrlLod2NoneTex);
-        startAR(tilesetUrlLod2NoneTex);
-        setIsARStarted(true);
-      } else {
-        const tilesetUrlLod1 = plateauDatasetItems.find(({ lod }) => lod == 1).url
-        if (tilesetUrlLod1) {
-          console.log("LOD1 Tileset Exists: ", tilesetUrlLod1);
-          startAR(tilesetUrlLod1);
-          setIsARStarted(true);
-        }
-      }
-    }
+    if (!cesiumLoaded || !initialTilesetUrls) { return; }
+    startAR(initialTilesetUrls);
+    setIsARStarted(true);
+  
     return () => {
       stopAR();
       setIsARStarted(false);
     };
-  }, [cesiumLoaded, data]);
+  }, [cesiumLoaded, initialTilesetUrls]);
 
   // TODO: View3.0からdatasetが全く選択されない状態でもAR Viewは起動できるので、ARView側でもデータセット検索機能は必要なのでパネルのフル機能で実装する
   // 一旦はURLからの表示と検索が動けばSTG出せる。データセットパネルの中での詳細なモデルのプロパティ操作にも追って対応必要
