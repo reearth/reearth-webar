b7c3377d0c1431e801c18c3baf992a6ba46e63f5
Masaya Tsuruta <seinwesen@gmail.com>
2024-03-10 18:22:53 +0900

    クエパラでデータセットIDが渡ってくるパターンのライフサイクル整理

--- src/ARView.tsx
+++ src/ARView.tsx
@@ -7,24 +7,23 @@ import { PlateauDataset, PlateauDatasetItem } from "./components/shared/graphql/
 import { rootLayersAtom } from "./components/shared/states/rootLayer";
 import { compassBiasAtom, fovPiOverAtom } from "./components/prototypes/view/states/ar";
 
-const tilesetUrls = (nodes) => {
-  nodes.map(node => {
-    const plateauDataset = node;
+function tilesetUrls(plateauDatasets: [PlateauDataset]): string[] {
+  return plateauDatasets.map(plateauDataset => {
     const plateauDatasetItems = plateauDataset.items as [PlateauDatasetItem];
     // LOD2(テクスチャあり)->LOD2(テクスチャなし)->LOD1の順でフォールバック
     const tilesetUrlLod2Tex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "TEXTURE").url
     if (tilesetUrlLod2Tex) {
-      console.log("LOD2 with Texture Tileset Exists: ", tilesetUrlLod2Tex);
+      // console.log("LOD2 with Texture Tileset Exists: ", tilesetUrlLod2Tex);
       return tilesetUrlLod2Tex;
     } else {
       const tilesetUrlLod2NoneTex = plateauDatasetItems.find(({ lod, texture }) => lod == 2 && texture == "NONE").url
       if (tilesetUrlLod2NoneTex) {
-        console.log("LOD2 with No Texture Tileset Exists: ", tilesetUrlLod2NoneTex);
+        // console.log("LOD2 with No Texture Tileset Exists: ", tilesetUrlLod2NoneTex);
         return tilesetUrlLod2NoneTex;
       } else {
         const tilesetUrlLod1 = plateauDatasetItems.find(({ lod }) => lod == 1).url
         if (tilesetUrlLod1) {
-          console.log("LOD1 Tileset Exists: ", tilesetUrlLod1);
+          // console.log("LOD1 Tileset Exists: ", tilesetUrlLod1);
           return tilesetUrlLod1;
         } else {
           return null;
@@ -35,34 +34,28 @@ const tilesetUrls = (nodes) => {
 }
 
 export default function ARView({...props}) {
-  // 初期化フラグ
-  const [cesiumLoaded, setCesiumLoaded] = useState(false);
-  const [isARStarted, setIsARStarted] = useState(false);
-
-  // データセットパネルに追加されたレイヤー群
-  const [rootLayers, setRootLayers] = useAtom(rootLayersAtom);
+  // TODO: View3.0からdatasetが全く選択されない状態でもAR Viewは起動できるので、ARView側でもデータセット検索機能は必要なのでパネルのフル機能で実装する
+  // 一旦はURLからの表示と検索が動けばSTG出せる。データセットパネルの中での詳細なモデルのプロパティ操作にも追って対応必要
+  // AR View 側でデータセットを変更した際に、リロードしてもそれが再現できるようにURLのクエパラもAR View側で書き換える機能は Nice to Have
 
   // 開始時にクエパラでデータセットIDを指定された場合にARViewの初期化に使用するtilesetURL
   let initialTilesetUrls: string[];
-
-  // クエパラを見てPLATEAU ViewからのデータセットIDの初期値が来ていれば取得
+  // クエパラを見てPLATEAU ViewからのデータセットID群の初期値が来ていれば取得し、tilesetURL群に変換
   const searchQueryParams = queryString.parse(location.search, {arrayFormat: 'comma'});
   let initialDatasetIds = searchQueryParams.id;
   if (typeof initialDatasetIds === 'string') { initialDatasetIds = [initialDatasetIds]; }
   if (initialDatasetIds) {
-    console.log(initialDatasetIds);
-    // const initialDatasets = useDatasetsByIds(initialDatasetIds).data;
-    // console.log(initialDatasets);
-    const initialDatasets = useDatasetById(initialDatasetIds[0]).data;
-    console.log(initialDatasets);
-    initialTilesetUrls = tilesetUrls(initialDatasetIds);
+    const { data } = useDatasetsByIds(initialDatasetIds);
+    const initialPlateauDatasets = data.nodes as [PlateauDataset];
+    // useDatasetsByIdsクエリが中身のあるデータを返してくるまでは待機
+    if (initialPlateauDatasets) {
+      initialTilesetUrls = tilesetUrls(initialPlateauDatasets);
+      console.log(initialTilesetUrls);
+    }
   }
 
-  // UIのステート
-  const [compassBias] = useAtom(compassBiasAtom);
-  const [fovPiOver] = useAtom(fovPiOverAtom);
-
   // CDNからCesiumを読み込むバージョン
+  const [cesiumLoaded, setCesiumLoaded] = useState(false);
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://cesium.com/downloads/cesiumjs/releases/1.114/Build/Cesium/Cesium.js';
@@ -74,20 +67,14 @@ export default function ARView({...props}) {
       document.body.removeChild(script);
     };
   }, []);
-
   // パッケージからCesiumを読込む場合は単にこれでOK
   // useEffect(() => {
   //   startAR();
   //   return () => stopAR();
   // }, []);
 
-  // データセットパネルのレイヤー群が変化したらARViewを再レンダリング
-  // useEffect(() => {
-  //   const datasetIds = rootLayers.map(rootLayer => rootLayer.rawDataset.id);
-  //   setDatasetIds(datasetIds);
-  // }, [rootLayers]);
-
   // AR View 起動
+  const [isARStarted, setIsARStarted] = useState(false);
   useEffect(() => {
     if (!cesiumLoaded || !initialTilesetUrls) { return; }
     startAR(initialTilesetUrls);
@@ -99,11 +86,17 @@ export default function ARView({...props}) {
     };
   }, [cesiumLoaded, initialTilesetUrls]);
 
-  // TODO: View3.0からdatasetが全く選択されない状態でもAR Viewは起動できるので、ARView側でもデータセット検索機能は必要なのでパネルのフル機能で実装する
-  // 一旦はURLからの表示と検索が動けばSTG出せる。データセットパネルの中での詳細なモデルのプロパティ操作にも追って対応必要
-  // AR View 側でデータセットを変更した際に、リロードしてもそれが再現できるようにURLのクエパラもAR View側で書き換える機能は Nice to Have
+  // データセットパネルに追加されたレイヤー群
+  const [rootLayers, setRootLayers] = useAtom(rootLayersAtom);
+  // データセットパネルのレイヤー群が変化したらARViewを再レンダリング
+  // useEffect(() => {
+  //   const datasetIds = rootLayers.map(rootLayer => rootLayer.rawDataset.id);
+  //   setDatasetIds(datasetIds);
+  // }, [rootLayers]);
 
   // UIのステート変更を監視してVMに反映
+  const [compassBias] = useAtom(compassBiasAtom);
+  const [fovPiOver] = useAtom(fovPiOverAtom);
   useEffect(() => {
     if (!isARStarted) { return; }
     console.log("compass bias (UI): ", compassBias);
