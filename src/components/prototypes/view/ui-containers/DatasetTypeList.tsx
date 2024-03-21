6371776cab0a5c59da54d49c6ab38eba554e300a
Shinnosuke Komiya <>
2024-03-21 12:53:57 -0700

    カテゴリータブの表示を修正

--- src/components/prototypes/view/ui-containers/DatasetTypeList.tsx
+++ src/components/prototypes/view/ui-containers/DatasetTypeList.tsx
@@ -102,14 +102,15 @@ const DatasetTypeItem: FC<{ datasetType: PlateauDatasetType }> = ({ datasetType 
   const query = useAreas({
     datasetTypes: [datasetType],
   });
+  // TODO: 対応地物が増えればここを増やして対応。なおなぜか今も建築物モデルはどの都道府県も表示されない
+  if(datasetType !== PlateauDatasetType.Building) return;
   return (
     <DatasetTreeItem
       nodeId={datasetType}
       label={datasetTypeNames[datasetType]}
       loading={query.loading}>
-      {datasetType === PlateauDatasetType.UseCase && <GlobalItem />}
-      {/* TODO: 対応地物が増えればここを増やして対応。なおなぜか今も建築物モデルはどの都道府県も表示されない */}
-      {datasetType === PlateauDatasetType.Building && query.data?.areas.map(
+      {/* {datasetType === PlateauDatasetType.UseCase && <GlobalItem />} */}
+      {query.data?.areas.map(
         prefecture =>
           prefecture.__typename === "Prefecture" && (
             <PrefectureItem
